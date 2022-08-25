a6842720bf70fe2c5d5c1cc331bcfd456b3491d3
Rolo2796 <48720104+R0L02796@users.noreply.github.com>
2022-08-25 18:40:59 -0300

    first commit

--- main.py
+++ main.py
@@ -1,23 +1,20 @@
 import numpy as np
 import node
 
-
 colors = 5
 dim = 6
 
-
 # arrays para movernos hacia los bloques adyacentes
-#index = 0 ---> ARRIBA
-#index = 1 ---> IZQUIERDA
-#index = 2 ---> DERECHA
-#index = 3 ---> ABAJO
-row = [ -1, 0, 0, 1]
-col = [ 0, -1, 1, 0 ]
+# index = 0 ---> ARRIBA
+# index = 1 ---> IZQUIERDA
+# index = 2 ---> DERECHA
+# index = 3 ---> ABAJO
+row = [-1, 0, 0, 1]
+col = [0, -1, 1, 0]
 
 
 # devuelve una matriz con 1's donde se ubica nuestra isla principal
-def get_main_island_rec(matrix, visited,  i, j, color):
-
+def get_main_island_rec(matrix, visited, i, j, color):
     if i < 0 or j < 0 or i >= dim or j >= dim or visited[i][j]:
         return visited
 
@@ -33,22 +30,19 @@ def get_main_island_rec(matrix, visited,  i, j, color):
     return visited
 
 
-
 # cambia el color de la isla principal
 def change_color(matrix, visited, color):
     for i in range(dim):
         for j in range(dim):
-            if(visited[i][j]):
-                matrix[i][j]=color
+            if visited[i][j]:
+                matrix[i][j] = color
     return matrix
-
-
 
 
 def is_goal(node):
     for i in range(dim):
         for j in range(dim):
-            if(node.state[i][j]!=node.color):
+            if node.state[i][j] != node.color:
                 return False
     return True
 
@@ -56,14 +50,14 @@ def is_goal(node):
 def is_insignificant_move(visited, new_visited):
     for i in range(dim):
         for j in range(dim):
-            if(visited[i][j]!=new_visited[i][j]):
+            if visited[i][j] != new_visited[i][j]:
                 return False
     return True
 
+
 # armo una cola y voy sacando el nodo que hace más tiempo se encuentra en la cola
 def bfs(root):
-    queue = []
-    queue.append(root)
+    queue = [root]
 
     while queue:
         actual_node = queue.pop(0)
@@ -76,36 +70,69 @@ def bfs(root):
         for color in range(colors):
             if color != actual_node.color:
                 new_state = change_color(actual_node.state, actual_node.visited, color)
-                visited = np.zeros ( (dim , dim) )
-                visited = get_main_island_rec ( new_state , visited , 0 , 0 , color )
-                if not is_insignificant_move(actual_node.visited, visited) :
-                    print ( '                 ' )
-                    print ( new_state )
-                    new_node=node.Node(new_state,visited, actual_node.cost + 1, actual_node, color)
+                blank_matrix = np.zeros((dim, dim))
+                main_island = get_main_island_rec(new_state, blank_matrix, 0, 0, color)
+                if not is_insignificant_move(actual_node.visited, main_island):
+                    print('                 ')
+                    print(new_state)
+                    new_node = node.Node(new_state, main_island, actual_node.cost + 1, actual_node, color)
                     queue.append(new_node)
 
 
+def dfs(actual_node):
+    if is_goal(actual_node):
+        return actual_node
+    for color in range(colors):
+        if color != actual_node.color:
+            new_state = change_color(actual_node.state, actual_node.visited, color)
+            new_node = node.Node(new_state, false , actual_node.cost + 1, node, color)
+            dfs(new_node)
 
 
+def dfs_non_recursive(graph, source):
+    if source is None or source not in graph:
+        return "Invalid input"
+    path = []
+    stack = [source]
+    while (len(stack) != 0):
+        s = stack.pop()
+        if s not in path:
+            path.append(s)
+        if s not in graph:
+            # leaf node
+            continue
+        for neighbor in graph[s]:
+            stack.append(neighbor)
+    return " ".join(path)
+
+
+def recursive_dfs(graph, source, path=[]):
+    if source not in path:
+        path.append(source)
+        if source not in graph:
+            # leaf node, backtrack
+            return path
+        for neighbour in graph[source]:
+            path = recursive_dfs(graph, neighbour, path)
+    return path
 
 
 def main():
     random_matrix = np.random.randint(0, colors, (dim, dim))
 
-    visited = np.zeros ( (dim , dim) )
+    visited = np.zeros((dim, dim))
 
-    root = node.Node(random_matrix, get_main_island_rec ( random_matrix , visited , 0 , 0 , random_matrix[0][0] ), 0, None, random_matrix[0][0])
-    #print(root.visited)
+    root = node.Node(random_matrix, get_main_island_rec(random_matrix, visited, 0, 0, random_matrix[0][0]), 0, None,
+                     random_matrix[0][0])
+    # print(root.visited)
 
-    #print(bfs(root))
+    # print(bfs(root))
     print('Initial state')
     print(random_matrix)
-    print ( '                 ' )
-
+    print('                 ')
 
     bfs(root)
 
 
-
 if __name__ == "__main__":
- main()
+    main()
