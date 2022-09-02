fdb6e651f7acfffde9f2bf66b1ef49a02d707a23
fzimbimbakis <fzimbimbakis@itba.edu.ar>
2022-09-02 18:42:20 -0300

    Agrego algoritmo greedy y la primer heuristica admisible.

--- main.py
+++ main.py
@@ -1,10 +1,11 @@
 import numpy as np
 import node
 import Utils.priorityQueue as priorityQueue
+import time
 
 colors = 5
-dim = 6
-heuristic=1
+dim = 10
+heuristic = 1
 
 # arrays para movernos hacia los bloques adyacentes
 # index = 0 ---> ARRIBA
@@ -13,7 +14,7 @@ heuristic=1
 # index = 3 ---> ABAJO
 row = [-1, 0, 0, 1]
 col = [0, -1, 1, 0]
-movement_cost = 1;
+movement_cost = 1
 
 
 # devuelve una matriz con 1's donde se ubica nuestra isla principal
@@ -42,10 +43,10 @@ def change_color(matrix, visited, color):
     return matrix
 
 
-def is_goal(node):
+def is_goal(actual_node):
     for i in range(dim):
         for j in range(dim):
-            if node.state[i][j] != node.color:
+            if actual_node.state[i][j] != actual_node.color:
                 return False
     return True
 
@@ -76,8 +77,8 @@ def bfs_search(root):
                 blank_matrix = np.zeros((dim, dim))
                 main_island = get_main_island_rec(new_state, blank_matrix, 0, 0, color)
                 if not is_insignificant_move(actual_node.visited, main_island):
-                    print('                 ')
-                    print(new_state)
+                    # print('                 ')
+                    # print(new_state)
                     new_node = node.Node(new_state, main_island, actual_node.cost + 1, actual_node, color)
                     queue.append(new_node)
 
@@ -105,14 +106,24 @@ def dfs_search(actual_node, limit):
                     return next_node
 
 
-#funcion heurisitca que devuelve la cantidad de bloques nuevos 'conquistados'
-def heuristic1(node):
-    return 0
+# funcion heuristica que devuelve la cantidad de colores restantes para poder
+# conquistar toda la matriz (no se tiene en cuenta la isla principal)
+def heuristic1(actual_node):
+    color_list = []
+    for i in range(dim):
+        for j in range(dim):
+            if actual_node.visited[i][j] == 0 and actual_node.state[i][j] not in color_list:
+                    color_list.append(actual_node.state[i][j])
+            if color_list.__len__() == colors:
+                break
+        if color_list.__len__() == colors:
+            break
+    return color_list.__len__()
 
-#funcion heuristica ??
-def heuristic2(node):
-    return 0
 
+# ¿funcion heuristica?
+def heuristic2(actual_node):
+    return 0
 
 
 def a_search(root):
@@ -137,8 +148,8 @@ def a_search(root):
                     print(new_state)
                     new_node = node.Node(new_state, main_island, actual_node.cost + 1, actual_node, color)
 
-                    if(heuristic == 1):
-                        heuristic_val= heuristic1(new_node)
+                    if heuristic == 1:
+                        heuristic_val = heuristic1(new_node)
                     else:
                         heuristic_val = heuristic2(new_node)
 
@@ -146,12 +157,43 @@ def a_search(root):
                     queue.insert(new_node)
 
 
+def greedy(root):
+    current = root
+    while not is_goal(current):
+        queue = priorityQueue.PriorityQueue()
+
+        # por cada color veo como queda la matriz al escogerlo
+        for color in range(colors):
+            if color != current.color:
+                aux = np.copy(current.state)
+                new_state = change_color(aux, current.visited, color)
+                blank_matrix = np.zeros((dim, dim))
+                main_island = get_main_island_rec(new_state, blank_matrix, 0, 0, color)
+                if not is_insignificant_move(current.visited, main_island):
+                    # print('                 ')
+                    # print(new_state)
+                    new_node = node.Node(new_state, main_island, current.cost + 1, current, color)
+
+                    if heuristic == 1:
+                        heuristic_val = heuristic1(new_node)
+                    else:
+                        heuristic_val = heuristic2(new_node)
+
+                    new_node.set_value(heuristic_val)
+                    queue.insert(new_node)
+        current = queue.pop()
+
+    print('GOAL')
+    return current
+
+
 def main():
     random_matrix = np.random.randint(0, colors, (dim, dim))
 
     visited = np.zeros((dim, dim))
 
-    root = node.Node(random_matrix, get_main_island_rec(random_matrix, visited, 0, 0, random_matrix[0][0]), 0, None,
+    root = node.Node(random_matrix,
+                     get_main_island_rec(random_matrix, visited, 0, 0, random_matrix[0][0]), 0, None,
                      random_matrix[0][0])
     # print(root.visited)
 
@@ -160,10 +202,15 @@ def main():
     print(random_matrix)
     print('                 ')
 
-    # bfs_search(root)
-    goal = dfs_search(root, 10)
+    start = time.time()
+    goal = bfs_search(root)
+    # goal = dfs_search(root, 100)
+    # goal = greedy(root)
+    end = time.time()
+    print(str(end - start) + ' secs')
     current = goal
-    while current is not root:
+    while current is not None:
+        # print('#colors=' + str(heuristic1(current)))
         print(current.state)
         print('                 ')
         current = current.parent
